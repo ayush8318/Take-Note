5c93f3524b57d2de2ffc299a5a1882e08feeadc2
ayush8318 <bt21eee019@nituk.ac.in>
2024-09-04 17:09:26 +0530

    update the mongo uri

--- Backend/db.js
+++ Backend/db.js
@@ -1,14 +1,16 @@
-const mongoose=require('mongoose');
+const mongoose = require("mongoose");
 
-// const mongoURI="mongodb://localhost:27017"; this connection string from mongodb compass
+const mongoURI =
+  "mongodb+srv://ayush:k_4._5.3nb2Tc5C@cluster0.ua0w0.mongodb.net/takenotes";
 
- 
 // add inotebook to mongoodb
-const mongoURI="mongodb://localhost:27017/inotebook";
-
+// const mongoURI = "mongodb://localhost:27017/inotebook";
 
 async function connectToMongo() {
-    await mongoose.connect(mongoURI).then(()=> console.log("Connected to Mongo Successfully")).catch(err => console.log(err));
-  }
-  
-  module.exports = connectToMongo;
+  await mongoose
+    .connect(mongoURI)
+    .then(() => console.log("Connected to Mongo Successfully"))
+    .catch((err) => console.log(err));
+}
+
+module.exports = connectToMongo;

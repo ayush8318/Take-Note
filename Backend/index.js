8a44f7856dc1c3b96e331796a019dda6d71aae3e
ayush8318 <bt21eee019@nituk.ac.in>
2024-09-04 15:53:52 +0530

    fix cors

--- Backend/index.js
+++ Backend/index.js
@@ -1,30 +1,30 @@
-const connectToMongo=require('./db')
+const connectToMongo = require("./db");
 
+var cors = require("cors");
+const express = require("express");
+const app = express();
 
- 
-var cors = require('cors')
- 
 connectToMongo();
-//above two lines are used to connect with mongodb 
-
+//above two lines are used to connect with mongodb
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 // from express.js google
-const express = require('express')
-const app = express()
-const port = 5000
+const port = 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello world ayush')
-})
- 
+app.get("/", (req, res) => {
+  res.send("Hello world ayush");
+});
+
 // Available routes
- 
 
-app.use(cors())
-app.use(express.json());//with the help of this line we are able to use req.body
+app.use(cors());
+app.use(express.json()); //with the help of this line we are able to use req.body
 
-
-app.use('/api/auth',require('./routes/auth'))
-app.use('/api/notes',require('./routes/notes'))
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
 app.listen(port, () => {
-  console.log(`iNotebook backened listening on port ${port}`)
-})
+  console.log(`iNotebook backened listening on port ${port}`);
+});
